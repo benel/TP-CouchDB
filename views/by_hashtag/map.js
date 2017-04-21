04c19460b8d64ee032b37e14a7e887cc098e44b5
Aurelien Benel <aurelien.benel@utt.fr>
2017-04-21 09:53:58 +0200

    REFACTORING: Simplified API to get tweets by hashtags
    in reverse chronological order.
    
    Route: /hashtag/:hashtag

--- views/by_hashtag/map.js
+++ views/by_hashtag/map.js
@@ -2,7 +2,7 @@ function(doc) {
   const time = new Date(doc.created_at).getTime();
   if (doc.text) {
     for each (h in doc.text.match(/#\w+/g)) {
-      emit([h, time]);
+      emit([h.substring(1), time]);
     }
   }
 }
