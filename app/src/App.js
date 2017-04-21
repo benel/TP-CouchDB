7681f60231db165e07fbacd991dbd67d4842304b
Aurelien Benel <aurelien.benel@utt.fr>
2017-04-21 19:13:26 +0200

    FEATURE: Add a new tweet.
    TODO: Post in on the API.

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import jQuery from 'jquery'; 
 import moment from 'moment'; 
+import uuid from 'uuid';
 import logo from './logo.svg';
 import './App.css';
 import 'moment/locale/fr';
@@ -33,6 +34,7 @@ class Feed extends React.Component {
     const tweets = this._getTweets();
     return (
       <div className="feed">
+        <TweetForm addTweet={this._addTweet.bind(this)} />
         {tweets}
       </div>
     );
@@ -48,6 +50,15 @@ class Feed extends React.Component {
 
   componentWillUnmount() {
     clearInterval(this._timer);
+  }
+
+  _addTweet(user, text) {
+    const doc = {
+      _id: uuid(),
+      user,
+      text
+    };
+    this.setState({tweets: [{doc}].concat(this.state.tweets)});
   }
 
   _fetchTweets() {
@@ -78,6 +89,29 @@ class Tweet extends React.Component {
         <div className="text">{this.props.text}</div>
       </div>
     );
+  }
+}
+
+class TweetForm extends React.Component {
+  render() {
+    return (
+      <form className="tweet-form" onSubmit={this._handleSubmit.bind(this)}>
+        <div>
+          <input placeholder="Qui êtes-vous ?" ref={(x) => this._user = x} />
+        </div>
+        <div>
+          <input className="new-tweet" placeholder="Quoi de neuf ?" ref={(x) => this._text = x} />
+          <button type="submit">Publier</button>
+        </div>
+      </form>
+    );
+  }
+
+  _handleSubmit(event) {
+    event.preventDefault();
+    this.props.addTweet(this._user.value, this._text.value);
+    this._user.value = null;
+    this._text.value = null;
   }
 }
 
